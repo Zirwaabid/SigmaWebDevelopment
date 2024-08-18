f84e821861397f86a8e1050f542a1f4fc33ea88f
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-17 18:45:34 -0700

    new

--- JavaScript/lecture 57/index.js
+++ JavaScript/lecture 57/index.js
@@ -5,74 +5,74 @@ console.log("Lecture about loops")
 let a=1;
 
 // for loop
-// for (let i = 0; i < 30 ; i++) {
-//     console.log(a+i)
+for (let i = 0; i < 30 ; i++) {
+    console.log(a+i)
     
-// }
+}
 // Quiz
-// let grace=1;
-// for (let age = 0; age < 7; age++) {
-//     console.log(grace+age)
+let grace=1;
+for (let age = 0; age < 7; age++) {
+    console.log(grace+age)
     
-// }
+}
 
 
-// let obj={
-//     name:"Zirwa",
-//     role:"programmer",
-//     age:19,
-// }
+let obj={
+    name:"Zirwa",
+    role:"programmer",
+    age:19,
+}
 // for in loop 
-// for (const key in obj) { {
-//         const element = obj[key];
-//         console.log(key,element)
+for (const key in obj) { {
+        const element = obj[key];
+        console.log(key,element)
         
-//     }
-// }
+    }
+}
 // Quiz
-// let object={
-//     class:6,
-//     "student name":"Amna",
-//     marks:10,
-// }
-// for (const key in object) { 
-//         const element = object[key];
-//         console.log(key,element)
-// }
+let object={
+    class:6,
+    "student name":"Amna",
+    marks:10,
+}
+for (const key in object) { 
+        const element = object[key];
+        console.log(key,element)
+}
 
 // for of loop
-// for (const c of "Zirwa") {
-//     console.log(c)
+for (const c of "Zirwa") {
+    console.log(c)
     
-// }
+}
 // quiz
-// for (const character of "ALLAH") {
-//     console.log(character)
-// }
+for (const character of "ALLAH") {
+    console.log(character)
+}
 
 // while loop
-// let i=0;
-// while (i<6) {
-//     console.log(i)
-//     i++;
-// }
+let c=0;
+while (c<6) {
+    console.log(c)
+    c++;
+}
 // quiz
-// let b=2;
-// while (b<5) {
-//     console.log(b);
-//     b++;
+let d=2;
+while (d<5) {
+    console.log(d);
+    d++;
     
-// }
+}
 
 // do while loop
-// let i=5;
-// do {
-//     console.log(i);
-//     i++;
-// } while (i<5);
+let i=5;
+do {
+    console.log(i);
+    i++;
+} while (i<5);
 // quiz
-// let b=5;
-// do {
-//     console.log(b);
-//      b++;
-// } while ((b<10) );
+let b=5;
+do {
+    console.log(b);
+     b++;
+} while ((b<10) );
