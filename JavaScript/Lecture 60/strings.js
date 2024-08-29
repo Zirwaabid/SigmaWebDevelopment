55ef4a14ca279e71384e264510d600209712e31d
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-29 13:44:10 -0700

    Lecture 60

---
+++ JavaScript/Lecture 60/strings.js
@@ -0,0 +1,12 @@
+console.log("This tutorial is about strings");
+let a="Zirwa";
+
+//how to access letters separately
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+//how to access array lenght
+console.log(a.length)
