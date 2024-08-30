1e81d23a728bfe30f853b905b80283d647caee2a
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-29 17:32:09 -0700

    Lecture 60

--- JavaScript/Lecture 60/strings.js
+++ JavaScript/Lecture 60/strings.js
@@ -10,3 +10,23 @@ console.log(a[4]);
 
 //how to access array lenght
 console.log(a.length)
+
+//template literals
+let name="Zirwa";
+let friend="Anas";
+console.log("Her name is "+ name+" and her friend name is "+friend+"") //difficult way
+console.log(`Her name is ${name} and her friend name is ${friend}`); //easy way by using template literals
+
+let b="Cat";
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+
+//slice
+let fruit="mango";
+console.log(fruit.slice(1,4))
+
+//replace
+console.log(fruit.replace("mango","orange"))
+
+//concat
+console.log(fruit.concat(name,friend,"Azra","abid"))
