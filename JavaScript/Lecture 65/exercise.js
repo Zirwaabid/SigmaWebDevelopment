864a0d961c80b1eb6498c0d41e9f8acb79244568
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-31 20:45:07 -0700

    Lecture 65

---
+++ JavaScript/Lecture 65/exercise.js
@@ -0,0 +1,10 @@
+// find factorial of 6 using 2 methods
+
+// 1-- by using reduce 
+let arr = [1, 2, 3, 4, 5, 6];
+const factorial = (a, b) => {
+    return a * b
+}
+console.log(arr.reduce(factorial));
+
+// 2--- by using for loop
