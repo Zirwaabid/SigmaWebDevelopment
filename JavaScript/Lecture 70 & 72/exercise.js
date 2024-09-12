4e407ccb76a369b340d687ab6558aeba5764848b
Zirwa Abid <zirwaabid112@gmail.com>
2024-09-11 17:11:08 -0700

    Lecture72

--- JavaScript/Lecture 70 & 72/exercise.js
+++ JavaScript/Lecture 70 & 72/exercise.js
@@ -1,7 +1,12 @@
-const boxes=document.querySelectorAll(".box");
-let random=Math.random();
-boxes.forEach(box=>{
- if (random>0.9) {
-    
- }
+const boxes = document.querySelector(".container").children;
+function GetRandomColor() {
+    let val1 = Math.ceil(0 + Math.random() * 255);
+    let val2 = Math.ceil(0 + Math.random() * 255);
+    let val3 = Math.ceil(0 + Math.random() * 255);
+    return `rgb(${val1}, ${val2}, ${val3})`
+}
+
+Array.from(boxes).forEach((box) => {
+    box.style.backgroundColor = GetRandomColor();
+    box.style.color = GetRandomColor();
 })
