0ac737d901e099823111610783de09da8968badd
Zirwa Abid <zirwaabid112@gmail.com>
2024-09-13 10:59:10 -0700

    exercise

---
+++ JavaScript/Lecture 73 & 77/script.js
@@ -0,0 +1,5 @@
+// function CreateCard(title,cName,views,monthsOld) {
+// document.querySelector(".title").innerHTML = title;
+// document.querySelector(".details").innerHTML =cName,views,monthsOld ;
+// }
+// CreateCard("Introduction to Backend | Sigma Web Dev video #2", "CodewithHarry", "560000","7")
