be2c96893437f71b6669cbd8ace8b80123d415ee
Zirwa Abid <zirwaabid112@gmail.com>
2024-09-15 12:46:30 -0700

    Lecture 74

---
+++ JavaScript/Lecture 74/script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("btn")
+
+button.addEventListener("click", () => {
+    document.querySelector(".container").innerHTML = "<b>yayy! you were clicked</b> Enjoy your click!"
+})
