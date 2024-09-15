1709d4be97c32535407dae1fa2d7420d1551eebe
Zirwa Abid <zirwaabid112@gmail.com>
2024-09-14 17:53:59 -0700

    exercise

--- JavaScript/Lecture 73 & 77/script.js
+++ JavaScript/Lecture 73 & 77/script.js
@@ -1,5 +1,22 @@
-// function CreateCard(title,cName,views,monthsOld) {
-// document.querySelector(".title").innerHTML = title;
-// document.querySelector(".details").innerHTML =cName,views,monthsOld ;
-// }
-// CreateCard("Introduction to Backend | Sigma Web Dev video #2", "CodewithHarry", "560000","7")
+function CreateCard(title, cName, views, monthsOld, duration, thumbnail) {
+    let viewStr
+    if (views < 1000) {
+        viewStr = views / 1000 ;
+    } else if (views > 1000000) {
+        viewStr = views / 1000000 + "M";
+    } else {
+        viewStr = views / 1000 + "K";
+    }
+    let html = ` <div class="card">
+            <div class="image">
+                <img src="${thumbnail}">
+                 <div class="capsule">${duration}</div>
+            </div>
+            <div class="text">
+                <h1>${title}</h1>
+                <p>${cName} . ${viewStr} views . ${monthsOld} months ago </p>
+            </div>
+        </div>`
+    document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
+}
+CreateCard("Introduction to Backend | Sigma Web Dev video #2", "CodewithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw");
