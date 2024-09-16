2a31650d9a6435427ae2835416e908add5a8d7ba
Zirwa Abid <zirwaabid112@gmail.com>
2024-09-15 18:28:21 -0700

    lecture 74

--- JavaScript/Lecture 74/script.js
+++ JavaScript/Lecture 74/script.js
@@ -1,5 +1,13 @@
 const button = document.getElementById("btn")
 
-button.addEventListener("click", () => {
+button.addEventListener("dblclick", () => {
     document.querySelector(".container").innerHTML = "<b>yayy! you were clicked</b> Enjoy your click!"
+})
+
+button.addEventListener("contextmenu", () => {
+    alert("Dont hack us with right click")
+})
+
+document.addEventListener("keydown", (evt) => {
+   console.log(evt.key,evt.keyCode)
 })
